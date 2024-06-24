93d2705a7699cbdbbcb2b54b639e82e223215687
ElColorin <franciscolazcano178@gmail.com>
2024-06-24 17:38:59 -0400

    EV3
    base de datos a aws

--- adquisicion/settings.py
+++ adquisicion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xerx@80e_a=-sv18=4oeak23z4cm_cw7n2rch=jirws&y+ui9w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1']
+ALLOWED_HOSTS = [ '34.199.103.123', '127.0.0.1']
 
 
 # Application definition
