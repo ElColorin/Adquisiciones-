8e8bf8235452f33208323284715c905019b469af
ElColorin <franciscolazcano178@gmail.com>
2024-07-15 04:14:35 -0400

    api

--- sistema/api/serializers.py
+++ sistema/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from sistema.models import Post
+from sistema.models import Product
 from sistema.models import Carrito
 from rest_framework.response import Response
 from django.utils import timezone
@@ -11,6 +12,10 @@ class PostSerializer(ModelSerializer):
         model = Post
         fields = '__all__'
 
+class ProductosSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 class CarritoSerializer(ModelSerializer):
     class Meta:
